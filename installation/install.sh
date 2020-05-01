e845689295be4bd4c3615a61198770a914e01c2d
Sebastian Murphy <sebastianmurphy@gmail.com>
2020-05-01 10:28:01 -0500

    wip

--- installation/install.sh
+++ installation/install.sh
@@ -6,9 +6,9 @@ apt-get --assume-yes install libboost-all-dev
 cd installation_scripts/
 ./part1.bash |& tee part1.log
 ./part2.bash |& tee part2.log
-./part3.bash |& tee part3.log
-./part4.bash |& tee part4.log
-./part5.bash |& tee part5.log
+#./part3.bash |& tee part3.log
+#./part4.bash |& tee part4.log
+#./part5.bash |& tee part5.log
 apt-get --assume-yes install python-gdal
 ln /dev/null /dev/raw1394
 apt-get --assume-yes install libsm6
